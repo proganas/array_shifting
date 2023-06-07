bfee65ff3a7fd9e7022bf51e8b71700cf6cffd49
proganas <43722466+proganas@users.noreply.github.com>
2023-06-07 13:42:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,12 @@ $x = array(
     array('7', '8', '9')
 );
 
-array_unshift($x[0], $x[0][2]);
-array_pop($x[0]);
-
-array_unshift($x[1], $x[1][2]);
-array_pop($x[1]);
-
-array_unshift($x[1], $x[1][2]);
-array_pop($x[1]);
+foreach ($x as $key => $value) {
+    for ($i = 0; $i <= $key; $i++) {
+        array_unshift($x[$key], $x[$key][count($value) - 1]);
+        array_pop($x[$key]);
+    }
+}
 
 echo "<pre>";
 print_r($x);
